951f1bb5295bd118d84477044ae3155c127006d4
borisman <boris.deneuville@gmail.com>
2012-05-06 21:58:42 +0200

    photo

---
+++ barre_titre.php
@@ -0,0 +1,11 @@
+<div id = "TopBar">
+  <div class="fond">
+  <ul id="menu_horizontal_barre_titre" >
+    <li>
+      <a href = ""> 
+	Galerie
+      </a>
+    </li>
+</ul>
+  </div>
+</div>
---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+<meta name="Keywords" content="photography,software,photos,digital darkroom,gallery,image,photographer,adobe,photoshop,lightroom" >
+<meta name="generator" content="Adobe Photoshop Lightroom" >
+<title>Titre du site</title>
+<link rel="stylesheet" type="text/css" media="screen" title="Custom Settings" href="content/custom.css" >
+<link rel="stylesheet" type="text/css" media="screen" title="Custom Settings" href="./resources/css/master.css" >
+
+<!--[if lt IE 7.]> <script defer type="text/javascript" src="./resources/js/pngfix.js"></script> <![endif]-->
+<!--[if gt IE 6]> <link rel="stylesheet" href="./resources/css/ie7.css"></link> <![endif]-->
+<!--[if lt IE 7.]> <link rel="stylesheet" href="./resources/css/ie6.css"></link> <![endif]-->
+</head>
+<body>
+<div id = "TopBar">
+  <div class="fond">
+  <ul id="menu_horizontal_barre_titre" >
+    <li>
+      <a href = ""> 
+	Galerie
+      </a>
+    </li>
+</ul>
+  </div>
+</div>
+
+</div>
+</body>
+</html>
+
+
